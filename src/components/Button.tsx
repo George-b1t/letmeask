5ba742f55343b56e6750531fd554f76bc0cfae70
George-b1t <george.soares509@gmail.com>
2021-06-22 14:34:45 -0300

    created-home_created-new-room

---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import '../styles/button.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+
+function Button(props: ButtonProps) {
+  return (
+    <>
+      <button 
+        className='button'
+        {...props}
+      />
+    </>
+  );
+};
+
+export { Button };
---
+++ src/pages/Home.tsx
@@ -0,0 +1,48 @@
+import { useHistory } from 'react-router-dom';
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
+
+function Home() { 
+  const history = useHistory();
+
+  function navigateToNewRoom () {
+    history.push('/rooms/new');
+  };
+
+  return (
+    <div id='page-auth'>
+      <aside>
+        <img src={illustrationImg} alt="Perguntas e Repostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className='main-content'>
+          <img src={logoImg} alt="Letmeask" />
+          <button onClick={() => navigateToNewRoom()} className='create-room'>
+            <img src={googleIconImg} alt="" />
+            Crie sua sala com o Google
+          </button>
+          <div className='separator'>ou entre em uma sala</div>
+          <form>
+            <input 
+              type="text" 
+              placeholder='Digite o código da sala'
+            />
+            <Button>
+              Entrar na sala
+            </Button>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export { Home };
